65551a9d25bd61a6fbf7477a168e41c4b571b97f
kevin-cueva <unimagneill@gmail.com>
2021-12-10 02:00:32 -0500

    CAMBIO EN LA BASE DE DATOS

--- database/sql/tables.sql
+++ database/sql/tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS table_data( -- crea la table_data tasks si no existe 
 -- Esta tabla es para manejar las tareas del usuario
     id INTEGER PRIMARY KEY, --llave primaria 
-    temperatura FLOAT(2,1), --float
-    ph FLOAT(2,1), -- float
+    temperatura TEXT, --float
+    ph TEXT, -- float
     created_date TEXT NO NULL -- la fecha de creacion
 )
